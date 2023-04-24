63addcdd6d116e4346e26023843a5c4e7b281e49
Fixxoo <rajpalmal40@gmail.com>
2023-04-24 12:53:05 +0530

    Adding Nav animation while scrolling

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
+const navv = document.getElementById("nav");
+console.log(document.getElementById("body"));
+console.log(document.getElementById("body").scrollTop);
 
-const navv = document.getElementsByTagName('nav')
-console.log(document.getElementById('body'));
-console.log(document.getElementById('body').scrollTop);
+// const element = document.getElementById("body");
+// let y = element.scrollTop;
+
+// function myFunction() {
+//   const element = document.getElementById("nav");
+//   // document.getElementById ("demo").innerHTML = "Horizontally: " + x.toFixed() + "<br>Vertically: " + y.toFixed();
+//   console.log(y)
+//
 
 
-function myFunction() {
+// window.addEventListener('scroll', function() {
+//   document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+//   console.log();
+// });
+
+
+
+function scrollFunction() {
   const element = document.getElementById("body");
-  let x = element.scrollLeft;
-  // document.getElementById ("demo").innerHTML = "Horizontally: " + x.toFixed() + "<br>Vertically: " + y.toFixed();
-  console.log(x)
+  // let y = element.scrollTop;
+  let y = window.pageYOffset;
+  // console.log(y.toFixed())
+  // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (y > 50) {
+    navv.classList.add("bg-[#222]");
+    // console.log("yes");
+  } else {
+    navv.classList.remove("bg-[#222]");
+    // console.log('yes');
+  }
 }
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//       // navv.classList.add('bg-[#222]')
-//       console.log('yes');
-//   } else {
-//       // navv.classList.remove('bg-[#222]');
-//       console.log('yes');
-//   }
-// };
-
-
